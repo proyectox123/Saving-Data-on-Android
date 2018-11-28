ec2f82c249f10d75a5831b97cb5ac844befa9708
MHO <mauricio.hdez.o.10@gmail.com>
2018-11-28 15:05:59 -0600

    Reading and Writing SharedPrefs.

--- app/src/main/java/com/raywenderlich/android/datadrop/model/MapPrefs.kt
+++ app/src/main/java/com/raywenderlich/android/datadrop/model/MapPrefs.kt
@@ -4,6 +4,18 @@ import android.preference.PreferenceManager
 import com.raywenderlich.android.datadrop.app.DataDropApplication
 
 object MapPrefs {
+
+    private const val KEY_MARKER_COLOR = "KEY_MARKER_COLOR"
+
     fun sharedPrefs() =
             PreferenceManager.getDefaultSharedPreferences(DataDropApplication.getAppContext())
+
+    fun saveMarkerColor(markerColor: String){
+        val editor = sharedPrefs().edit()
+        editor.putString(KEY_MARKER_COLOR, markerColor).apply()
+        //apply - asynchronously
+        //commit - synchronously
+    }
+
+    fun getMarkerColor(): String = sharedPrefs().getString(KEY_MARKER_COLOR, "Red")
 }
