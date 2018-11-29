57ce1b6ec494f2bb8962c0fb6b00bb800991caed
MHO <mauricio.hdez.o.10@gmail.com>
2018-11-29 14:52:08 -0600

    Writing Data.

--- app/src/main/java/com/raywenderlich/android/datadrop/model/SQLiteRepository.kt
+++ app/src/main/java/com/raywenderlich/android/datadrop/model/SQLiteRepository.kt
@@ -1,13 +1,16 @@
 package com.raywenderlich.android.datadrop.model
 
+import android.content.ContentValues
 import com.raywenderlich.android.datadrop.app.DataDropApplication
+import com.raywenderlich.android.datadrop.model.DropDbSchema.DropTable
 
 class SQLiteRepository : DropRepository {
 
     private val database = DropDbHelper(DataDropApplication.getAppContext()).writableDatabase
 
     override fun addDrop(drop: Drop) {
-
+        val contentValues = getDropContentValues(drop)
+        database.insert(DropTable.NAME, null, contentValues)
     }
 
     override fun getDrops(): List<Drop> {
@@ -22,4 +25,7 @@ class SQLiteRepository : DropRepository {
 
     }
 
+    private fun getDropContentValues(drop: Drop) =  ContentValues().apply {
+        put(DropTable.Columns.ID, drop.id)
+    }
 }
