17924065753678c5e29782429ab633bf3e8edd46
MHO <mauricio.hdez.o.10@gmail.com>
2018-11-30 14:03:21 -0600

    Reading Data.

---
+++ app/src/main/java/com/raywenderlich/android/datadrop/model/DropCursorWrapper.kt
@@ -0,0 +1,18 @@
+package com.raywenderlich.android.datadrop.model
+
+import android.database.Cursor
+import android.database.CursorWrapper
+import com.google.android.gms.maps.model.LatLng
+import com.raywenderlich.android.datadrop.model.DropDbSchema.DropTable
+
+class DropCursorWrapper(cursor: Cursor): CursorWrapper(cursor) {
+
+    fun getDrop(): Drop{
+        val id = getString(getColumnIndex(DropTable.Columns.ID))
+        val latitude = getDouble(getColumnIndex(DropTable.Columns.LATITUDE))
+        val longitude = getDouble(getColumnIndex(DropTable.Columns.LONGITUDE))
+        val dropMessage = getString(getColumnIndex(DropTable.Columns.DROP_MESSAGE))
+
+        return Drop(LatLng(latitude, longitude), dropMessage, id)
+    }
+}
--- app/src/main/java/com/raywenderlich/android/datadrop/model/SQLiteRepository.kt
+++ app/src/main/java/com/raywenderlich/android/datadrop/model/SQLiteRepository.kt
@@ -1,8 +1,10 @@
 package com.raywenderlich.android.datadrop.model
 
 import android.content.ContentValues
+import android.util.Log
 import com.raywenderlich.android.datadrop.app.DataDropApplication
 import com.raywenderlich.android.datadrop.model.DropDbSchema.DropTable
+import java.io.IOException
 
 class SQLiteRepository : DropRepository {
 
@@ -14,7 +16,22 @@ class SQLiteRepository : DropRepository {
     }
 
     override fun getDrops(): List<Drop> {
-        return emptyList()
+        val drops = mutableListOf<Drop>()
+        val cursor = queryDrops(null, null)
+
+        try{
+            cursor.moveToFirst()
+            while (!cursor.isAfterLast){
+                drops.add(cursor.getDrop())
+                cursor.moveToNext()
+            }
+        }catch (e: IOException){
+            Log.e("SQLiteRepository", "Error reading drops")
+        }finally {
+            cursor.close()
+        }
+
+        return drops
     }
 
     override fun clearDrop(drop: Drop) {
@@ -30,5 +47,19 @@ class SQLiteRepository : DropRepository {
         put(DropTable.Columns.LATITUDE, drop.latLng.latitude)
         put(DropTable.Columns.LONGITUDE, drop.latLng.longitude)
         put(DropTable.Columns.DROP_MESSAGE, drop.dropMessage)
+    }
+
+    private fun queryDrops(where: String?, whereArgs: Array<String>?): DropCursorWrapper{
+        val cursor = database.query(
+                DropTable.NAME,
+                null,
+                where,
+                whereArgs,
+                null,
+                null,
+                null
+        )
+
+        return DropCursorWrapper(cursor)
     }
 }
