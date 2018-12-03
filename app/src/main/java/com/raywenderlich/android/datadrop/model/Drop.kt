639e0ab7045ba4fcf159894beb66ad695ce4f579
MHO <mauricio.hdez.o.10@gmail.com>
2018-12-03 14:33:35 -0600

    Entities and DAOs.

--- app/src/main/java/com/raywenderlich/android/datadrop/model/Drop.kt
+++ app/src/main/java/com/raywenderlich/android/datadrop/model/Drop.kt
@@ -31,14 +31,16 @@
 
 package com.raywenderlich.android.datadrop.model
 
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
 import com.google.android.gms.maps.model.LatLng
 import com.raywenderlich.android.datadrop.ui.map.MarkerColor
 import java.util.*
 
-
+@Entity(tableName = "drop_table")
 data class Drop(val latLng: LatLng,
                 val dropMessage: String,
-                val id: String = UUID.randomUUID().toString(),
+                @PrimaryKey val id: String = UUID.randomUUID().toString(),
                 val markerColor: Int = 0) {
   val latLngString = "%.6f, %.6f".format(latLng.latitude, latLng.longitude)
 }
